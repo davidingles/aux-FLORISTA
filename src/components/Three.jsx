288a01b4ea6f5f9078ab975eb745fa73fa4bbc5b
davidingles <davidingleslopez@gmail.com>
2025-03-28 15:50:26 +0100

    florista222

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -5,7 +5,7 @@ import { Stats, OrbitControls, Environment, useGLTF, Clone, Html, ContactShadows
 import { useControls } from 'leva';
 
 const Models = [
-  { title: 'SOYFLORISTA', url: './SOYFLORISTA.glb', miEscala: .71, miPosicion: 3 },
+  { title: 'SOYFLORISTA1', url: './SOYFLORISTA1.glb', miEscala: .71, miPosicion: 3 },
   // { title: 'CANARA2', url: './CANARA2.glb', miEscala: 0.5, miPosicion: 0 },
   // { title: 'CANARA3', url: './CANARA3.glb', miEscala: 0.5, miPosicion: 0 },
   // { title: 'CANARA4', url: './CANARA4.glb', miEscala: 0.5, miPosicion: 0 },
